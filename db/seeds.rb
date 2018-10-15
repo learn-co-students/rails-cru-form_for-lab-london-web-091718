40a2bf9b11f8ff53ef6433b680f4cec8cf7a15d1
deedeeh <dinahafez.8@gmail.com>
2018-10-15 10:36:52 +0100

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist1 = Artist.create(name: "Beyonce", bio: "A greate artist.")
+artist2 = Artist.create(name: "Rihanna", bio: "A very strong voice!")
+
+genre1 = Genre.create(name: "Pop")
+genre2 = Genre.create(name: "R&B")
+
+song1 = Song.create(name: "Run the world", artist_id: artist1, genre_id: genre1)
+song2 = Song.create(name: "Diamonds", artist_id: artist2, genre_id: genre2)
