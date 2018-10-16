76d5a1d02c163b345c2a55e6c2422a4d2bda4fe8
ssong-eu <sangyoon.song@gmail.com>
2018-10-16 15:12:13 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :artists, only: [:index, :new, :edit, :create, :update, :show], as: :artists
+  resources :genres, only: [:index, :new, :edit, :create, :update, :show], as: :genres
+  resources :songs, only: [:index, :new, :edit, :create, :update, :show], as: :songs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+ts = Artist.create(name: "Taylor Swift", bio: "A Country Girl")
+ls = Artist.create(name: "Lindsey Stirling", bio: "A Violin Girl")
+bts = Artist.create(name: "BTS", bio: "Bunch of Korean guys")
+
+country = Genre.create(name: "Country")
+classic = Genre.create(name: "Classic")
+kpop = Genre.create(name: "Kpop")
+
+idol = Song.create(name: "Idol")
+idol.artist = bts
+idol.genre = kpop
+idol.save
+
+sm = Song.create(name: "Shatter Me")
+sm.artist = ls
+sm.genre = classic
+sm.save
+
+love_story = Song.create(name: "Love Story")
+love_story.artist = ts
+love_story.genre = country
+love_story.save
